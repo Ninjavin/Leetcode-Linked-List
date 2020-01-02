166c19c92ae4cca1b3d7dea405d33daf2197357e
unknown <vinjain2006@gmail.com>
2020-01-02 15:04:54 +0530

    1108. Easy. Solved.

---
+++ Defanging an IP Address.cpp
@@ -0,0 +1,17 @@
+//Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+//A defanged IP address replaces every period "." with "[.]".
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result("");
+        for(char c: address){
+            if(isdigit(c))
+                result+=c;
+            else
+                result+="[.]";
+        }
+        return result;
+    }
+};
